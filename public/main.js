bba4defbb37f6ba5e50b2413d2948b2c4405de37
AHachey17 <ahachey17@gmail.com>
2023-04-28 23:34:21 -0400

    ...

--- public/main.js
+++ public/main.js
@@ -5,7 +5,7 @@ var comment = document.getElementsByClassName("enter");
 Array.from(thumbUp).forEach(function(element) {
       element.addEventListener('click', function(){
         const name = this.parentNode.parentNode.childNodes[1].innerText
-        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[3].innerText)
         fetch('messages/thumbUp', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
@@ -57,8 +57,7 @@ Array.from(trash).forEach(function(element) {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({
-            'name': name,
-            'comment': comment
+            'name': name
           })
         }).then(function (response) {
           window.location.reload()
